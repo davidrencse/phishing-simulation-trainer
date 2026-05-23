8efb38e67d7f40e6e69f62dd38f62209e7cf5e65
redarmr <david.ren@nyu.edu>
2026-05-23 13:12:07 -0400

    Add generated app file: src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Phishing Simulation Trainer</h1>
+      <p>Get ready to learn about phishing threats!</p>
+    </div>
+  );
+};
+
+export default App;
